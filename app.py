788233a974c96715e2f171ea6f493432dc49e2c5
TobiOdanye <tobi.odanye@encore-search.com>
2025-06-18 23:51:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -341,7 +341,7 @@ def get_candidate_companies(group):
             (group['Candidate Company'] == company1)
         ].sort_values('Candidate Experience', ascending=False)
 
-        if company1_prior_rows['Candidate ID' == 18404928:
+        if company1_prior_rows['Candidate ID'] == 18404928:
             st.dataframe(company1_prior_rows)
             st.dataframe(company1_prior_rows.sort_values('Candidate Experience', ascending=False).iloc[0])
         
